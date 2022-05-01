b2d89b72d1b89aa2e58dce64fb72e7b25822b112
Mattias <osth.mattias@gmail.com>
2022-05-01 12:25:35 +0900

    refactor & test messages

--- index.js
+++ index.js
@@ -1,81 +1,102 @@
-require('dotenv').config();
-const WebSocket = require('ws');
-const config = require('./config');
-const ethers = require('ethers');
-const PORT = 8777;
+require("dotenv").config();
+const WebSocket = require("ws");
+const config = require("./config");
+const ethers = require("ethers");
+const PORT = 4000;
 const server = new WebSocket.Server({ port: PORT });
+
+const DEX = {
+  uniswap: {
+    contract: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+    abi: config.UNI_V2_FAC_ABI,
+    node: process.env.ETH_NODE,
+  },
+  pancake: {
+    contract: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
+    abi: config.CAKE_V2_FAC_ABI,
+    node: process.env.BSC_NODE,
+  },
+  quickswap: {
+    contract: "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32",
+    abi: config.QS_V2_FAC_ABI,
+    node: process.env.POLYGON_NODE,
+  },
+  trader_joe: {
+    contract: "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10",
+    abi: config.JOE_V2_FAC,
+    node: process.env.AVAX_NODE,
+  },
+};
 
 const connections = [];
 
 initListeners();
 console.log(`Server started on port ${PORT}`);
 
-// TOOD FIX DOUBLE CLIENT CONNECTION???
-server.on('connection', socket => {
-
-  console.log('Client connected!');
+server.on("connection", (socket) => {
+  console.log("Client connected!");
   connections.push(socket);
   console.log(`${connections.length} connections`);
 
 
-  socket.on('message', msg => {
-    console.log(`message received ${msg}`);
-    // socket.send(`Echo: ${msg}`);
+  socket.on("message", (msg) => {
+    const parsed = msg.toString();
+    console.log(`message received ${parsed}`);
+    if (parsed === 'token') {
+      testListing(socket);
+    } else if (parsed === "err") {
+      throw new Error("Custom throw!");
+    }
   });
-
-
 });
 
-function listingListener(dex) {
-  let provider;
-  let v2FactoryAddress;
-  let abi;
-  let listingText;
-  switch (dex) {
-    case 'uniswap':
-      provider = new ethers.providers.WebSocketProvider(process.env.ETH_NODE);
-      v2FactoryAddress = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
-      abi = config.UNI_V2_FAC_ABI;
-      listingText = 'UNISWAP LISTING: ';
-      break;
-    case 'pancake':
-      provider = new ethers.providers.WebSocketProvider(process.env.BSC_NODE);
-      v2FactoryAddress = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73';
-      abi = config.CAKE_V2_FAC_ABI;
-      listingText = 'PANCAKE LISTING: ';
-      break;
-    case 'quickswap':
-      provider =
-        new ethers.providers.WebSocketProvider(process.env.POLYGON_NODE);
-      v2FactoryAddress = '0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32';
-      abi = config.QS_V2_FAC_ABI;
-      listingText = 'QUICKSWAP LISTING: ';
-      break;
-    default:
-      throw new Error('Unknown dex');
-  }
+function listingListener(dexId) {
+  const dexData = DEX[dexId];
+  const provider = new ethers.providers.WebSocketProvider(dexData.node);
+  const v2FactoryAddress = dexData.contract;
+  const abi = dexData.abi;
+  const listingText = `${dexId} LISTING: `;
 
   const contract = new ethers.Contract(v2FactoryAddress, abi, provider);
 
   contract.on(
-    'PairCreated',
+    "PairCreated",
     async (tokenAddress0, tokenAddress1, pair, listingNumber) => {
       const tokenContract0 = new ethers.Contract(
-        tokenAddress0, config.GENERIC_ERC20_ABI, provider);
+        tokenAddress0,
+        config.GENERIC_ERC20_ABI,
+        provider
+      );
       const tokenContract1 = new ethers.Contract(
-        tokenAddress1, config.GENERIC_ERC20_ABI, provider);
-      const [name0, name1] =
-        await Promise.all([tokenContract0.name(), tokenContract1.name()]);
+        tokenAddress1,
+        config.GENERIC_ERC20_ABI,
+        provider
+      );
 
-      const date = new Date().toISOString().split('.')[0];
+      let name0, name1;
+      try {
+        [name0, name1] = await Promise.all([
+          tokenContract0.name(),
+          tokenContract1.name(),
+        ]);
+      } catch (err) {
+        console.log("Failed to get token names");
+      }
+
+      const date = new Date().toISOString().split(".")[0];
       console.log(
-        date, listingText, tokenAddress0, tokenAddress1, pair,
-        '(' + name0 + ' - ' + name1 + ')');
-      console.log('\n');
+        date,
+        listingText,
+        tokenAddress0,
+        tokenAddress1,
+        pair,
+        "(" + name0 + " - " + name1 + ")"
+      );
+      console.log("\n");
 
       const listing = {
         date,
-        dex,
+        dexId,
         token0: {
           contract: tokenAddress0,
           name: name0,
@@ -87,13 +108,36 @@ function listingListener(dex) {
         pair,
       };
 
-      connections.forEach(socket => socket.send(JSON.stringify(listing)));
-    });
+      connections.forEach((socket) => socket.send(JSON.stringify(listing)));
+    }
+  );
 }
 
 function initListeners() {
-  console.log('Scanning for token listings...');
-  listingListener('uniswap');
-  listingListener('pancake');
-  listingListener('quickswap');
+  console.log("Scanning for token listings...");
+  listingListener("uniswap");
+  listingListener("pancake");
+  listingListener("quickswap");
+  listingListener("trader_joe");
+}
+
+function testListing(socket) {
+  console.log('Test listing triggered');
+  const date = new Date().toISOString().split(".")[0];
+  const dexId = 'test_dex';
+  const listing = {
+    date,
+    dexId,
+    token0: {
+      contract: '0xtester1',
+      name: 'token1',
+    },
+    token1: {
+      contract: '0xtester2',
+      name: 'token2',
+    },
+    pair: '0xpair',
+  };
+
+  connections.forEach((socket) => socket.send(JSON.stringify(listing)));
 }
